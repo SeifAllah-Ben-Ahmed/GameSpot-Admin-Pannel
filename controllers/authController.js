6452bcfd0d5556ef95d14c27932fa92134147fb8
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2022-04-12 02:37:17 +0200

    Upgrade Email Handler 🐱‍🏍🐱‍🏍

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
-const sendEmail = require('../utils/Email');
+const Email = require('../utils/Email');
 
 const generateToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -109,13 +109,9 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetUrl = `${req.protocol}://${req.get(
     'host'
   )}/api/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetUrl}.\nif you didn't forget your password, please ignore this email!`;
+
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    await new Email(user, resetUrl).sendPasswordReset();
     res
       .status(200)
       .json({ status: 'success', message: 'Token sent to email!' });
--- utils/Email.js
+++ utils/Email.js
@@ -1,23 +1,67 @@
+const htmlToText = require('html-to-text');
 const nodemailer = require('nodemailer');
+const pug = require('pug');
 
-const sendEmail = async (option) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.name = user.name;
+    this.url = url;
+    this.from = `Seif Allah <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOption = {
-    from: 'Seif Ben Ahmed <hello@tech-e-shop.com>',
-    to: option.email,
-    subject: option.subject,
-    text: option.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //SendGrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  await transporter.sendMail(mailOption);
-};
+  //Send the actual email
+  async send(template, subject) {
+    // Render HTML base on pug template
+    const html = pug.renderFile(`${__dirname}/../emails/${template}.pug`, {
+      name: this.name,
+      url: this.url,
+      subject,
+    });
+    //Define email options
+    const mailOption = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+      //HTML
+    };
+    // Create a transport and send email
+    await this.newTransport().sendMail(mailOption);
+  }
 
-module.exports = sendEmail;
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the NodeJs Family');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset Token (Valid for only 10 minutes)'
+    );
+  }
+}
+
+module.exports = Email;
