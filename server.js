f6067ea524a19bb48d812f13f1a3294d60cc8d28
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2022-06-02 03:08:15 +0200

    Deploy

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/attributes', attributeRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/'));
-  app.get('*', (req, res) => {
+  app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
